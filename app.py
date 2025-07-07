3a672a47d2a048b43c20f94b049c4f8437960f2d
Cid <137782638+thisiscid@users.noreply.github.com.>
2025-07-06 23:27:08 -0400

    create the bolt stuff

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# This sample slack application uses SocketMode
+# For the companion getting started setup guide, 
+# see: https://slack.dev/bolt-python/tutorial/getting-started 
+
+# Initializes your app with your bot token
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+
+# Listens to incoming messages that contain "hello"
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click Me"},
+                    "action_id": "button_click",
+                },
+            }
+        ],
+        text=f"Hey there <@{message['user']}>!",
+    )
+
+
+@app.action("button_click")
+def action_button_click(body, ack, say):
+    # Acknowledge the action
+    ack()
+    say(f"<@{body['user']['id']}> clicked the button")
+
+
+# Start your app
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
